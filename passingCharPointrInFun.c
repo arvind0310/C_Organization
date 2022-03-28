60de145af92359049851c02fa20c262d1c63a99f
arvind-wmp <arvindkumar@wingmanpartners.com>
2022-03-28 11:52:24 +0530

    changed files

--- passingCharPointrInFun.c
+++ passingCharPointrInFun.c
@@ -45,29 +45,91 @@
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
-void modify(char **double_pointer)
-{
-    //define a char pointer
-    char *pointer = malloc(5 * sizeof(char));
-    strcpy(pointer, "test");
-    printf("%s\n", pointer); //prints test
+// void modify(char **double_pointer)
+// {
+//     //define a char pointer
+//     char *pointer = malloc(5 * sizeof(char));
+//     strcpy(pointer, "test");
+//     printf("%s\n", pointer); //prints test
 
-    // point double_pointer to pointer
-    double_pointer = malloc(sizeof(char *));
-    *double_pointer = pointer;
-    printf("%s\n", *double_pointer); //prints test
-}
+//     // point double_pointer to pointer
+//     double_pointer = malloc(sizeof(char *));
+//     *double_pointer = pointer;
+//     printf("%s\n", *double_pointer); //prints test
+// }
 
-int main()
-{
-    //pointer to a pointer
-    char *double_pointer = NULL;
-    modify(&double_pointer);
-    printf("%s\n", double_pointer); // seg. fault >> it can be solved ..
-                                    //when not be allocating memory for double_pointer.
-    return 0;
-}
+// int main()
+// {
+//     //pointer to a pointer
+//     char *double_pointer = NULL;
+//     modify(&double_pointer);
+//     printf("%s\n", double_pointer); // seg. fault >> it can be solved ..
+//                                     //when not be allocating memory for double_pointer.
+//     return 0;
+// }
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <unistd.h>
+// char *fun(char *ptr) // this fun will return a pointer
+// {
+//     return ptr;
+// }
+
+// int main()
+// {
+//     char str[20] = "arvind";
+//     char *var;
+//     var = fun(str);
+//     printf("%s\n", var);
+
+//     printf("%c\n", *var);
+
+// }
+
+//++++++++++++++++++++ method:1 ++++++++++++++++++++++++++++++++++++++++++++
+
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <unistd.h>
+// void fun(char *ptr)
+// {
+//     printf("%s\n",ptr);
+
+//     printf("%c\n",*ptr);
+//     // *(ptr+1)='x';   /* seg fault */
+//     // printf("%s\n",ptr);
+// }
+// int main()
+// {
+//     char *str="arvind";
+//     fun(str);
+// }
+
+//++++++++++++++++++ method:2 +++++++++++++++++++++++++
+
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <unistd.h>
+// void fun(char **ptr)
+// {
+//     printf("%s\n",*ptr);
+//     printf("%c\n",**ptr);
+//     // **(ptr+1)='x';   /* seg fault */
+//     // printf("%s\n",*ptr);
+// }
+// int main()
+// {
+//     char *str="arvind";
+//     fun(&str);
+// }
--- test.c
+++ test.c
@@ -1,61 +1,3 @@
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include <unistd.h>
-// char *fun(char *ptr) // this fun will return a pointer
-// {
-//     return ptr;
-// }
-
-// int main()
-// {
-//     char str[20] = "arvind";
-//     char *var;
-//     var = fun(str);
-//     printf("%s\n", var);
-
-//     printf("%c\n", *var);
-
-// }
-
-//++++++++++++++++++++ method:1 ++++++++++++++++++++++++++++++++++++++++++++
-
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include <unistd.h>
-// void fun(char *ptr)
-// {
-//     printf("%s\n",ptr);
-
-//     printf("%c\n",*ptr);
-//     // *(ptr+1)='x';   /* seg fault */
-//     // printf("%s\n",ptr);
-// }
-// int main()
-// {
-//     char *str="arvind";
-//     fun(str);
-// }
-
-//++++++++++++++++++ method:2 +++++++++++++++++++++++++
-
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include <unistd.h>
-// void fun(char **ptr)
-// {
-//     printf("%s\n",*ptr);
-//     printf("%c\n",**ptr);
-//     // **(ptr+1)='x';   /* seg fault */
-//     // printf("%s\n",*ptr);
-// }
-// int main()
-// {
-//     char *str="arvind";
-//     fun(&str);
-// }
 
 //++++++++++++++++++++++++++++++++++++++++
 
@@ -96,19 +38,4 @@
 //     }
 // }
 
-//+++++++++++++++++++++++++++++++++++++++
-
-// #include <stdio.h>
-// int base(int a)
-// {
-//     if (a == 1)
-//         return 38;
-//     return 37;
-// }
-
-// int main()
-// {
-//     int n = base(1); // now base has returned value 38 
-//     printf("Base(1) is %d\n", n); // printing return value
-//     return n + 3; // will return 41 but you need to print if u want to see the o/p.
-// }
+//+++++++++++++++++++++++++++++++++++++++ 
